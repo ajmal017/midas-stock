31327ee7ef3b14e7aa2d4678fbc9b93a88909f7c
paroonk <paroonk@hotmail.com>
2020-01-24 16:12:32 +0700

    Initial commit

--- midas-stock/scraper.py
+++ midas-stock/scraper.py
@@ -8,14 +8,13 @@ from multiprocessing import Pool, cpu_count
 import investpy
 import pandas as pd
 import pandas_datareader.data as web
+import requests_cache
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from tqdm import tqdm
-
-import requests_cache
 
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.width', 10000)
@@ -25,10 +24,10 @@ dir_path = os.path.dirname(os.path.realpath(__file__)) + '\\'
 
 
 def scrape_investing(df_stock, from_date, to_date, symbol):
+    """ Function to get stock's OHLC data from investing.com """
     try:
         df = investpy.get_stock_historical_data(stock=df_stock.loc[symbol, 'Investing'], country='thailand', from_date=from_date, to_date=to_date, as_json=False, order='ascending')
         df.drop(labels='Currency', axis=1, inplace=True)
-        df.sort_index(ascending=True, inplace=True)
         df[['Return']] = df[['Close']].pct_change()
         df = df.round(6)
         df.to_csv(dir_path + 'data/investing/' + df_stock.loc[symbol, 'Filename'] + '.csv')
@@ -39,6 +38,7 @@ def scrape_investing(df_stock, from_date, to_date, symbol):
 
 
 def scrape_yahoo(df_stock, start, end, symbol):
+    """ Function to get stock's OHLC data from yahoo.com """
     try:
         expire_after = dt.timedelta(days=3)
         session = requests_cache.CachedSession(cache_name='cache', backend='sqlite', expire_after=expire_after)
@@ -56,6 +56,7 @@ def scrape_yahoo(df_stock, start, end, symbol):
 
 
 def scrape_jitta(df_stock, symbol):
+    """ Function to get stock's financial statement data from jitta.com """
     global driver
 
     try:
@@ -96,6 +97,7 @@ def scrape_jitta(df_stock, symbol):
 
 
 def get_data_to_df(driver, df):
+    """ Function to scrape data table in jitta.com to df """
     soup = BeautifulSoup(driver.page_source, features='lxml')
     table = soup.body.find('div', class_=re.compile('FactsheetTable__TableContainer'))
     div_list = [div for div in table]
@@ -119,6 +121,7 @@ def get_data_to_df(driver, df):
 
 
 def close_driver(i):
+    """ Function to close webdriver """
     global driver
     if driver is not None:
         driver.close()
@@ -138,18 +141,16 @@ if __name__ == "__main__":
 
     start = dt.datetime.strptime('30/12/2009', '%d/%m/%Y')
     end = dt.datetime.now() - dt.timedelta(days=1)
+    from_date = start.strftime('%d/%m/%Y')
+    to_date = end.strftime('%d/%m/%Y')
 
-    # Reformat SET Hist Data
-    df_set = pd.read_csv(dir_path + 'data/SET Index Historical Data.csv')
-    if df_set.columns.values.tolist() == ['Date', 'Price', 'Open', 'High', 'Low', 'Vol.', 'Change %']:
-        df_set.rename(columns={'Price': 'Close', 'Vol.': 'Volume'}, inplace=True)
-        df_set = df_set.loc[:, ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Change %']]
-        df_set.loc[:, 'Date'] = pd.to_datetime(df_set.loc[:, 'Date'])
-        for col in ['Open', 'High', 'Low', 'Close', 'Volume']:
-            df_set.loc[:, col] = df_set.loc[:, col].str.replace(',', '')
-        df_set.loc[:, 'Volume'] = df_set.loc[:, 'Volume'].str.replace('.', '').str.replace('B', '0000000').str.replace('M', '0000')
-        df_set = df_set.set_index('Date').resample('D').fillna(method='ffill').sort_index(ascending=True).reset_index()
-        df_set.to_csv('data/SET Index Historical Data.csv', index=False)
+    # Scrape SET Hist Data
+    for index in ['SET', 'SET TRI']:
+        df = investpy.get_index_historical_data(index=index, country='thailand', from_date=from_date, to_date=to_date, as_json=False, order='ascending')
+        df.drop(labels='Currency', axis=1, inplace=True)
+        df[['Return']] = df[['Close']].pct_change()
+        df = df.round(6)
+        df.to_csv(dir_path + 'data/' + index + '.csv')
 
     # Scrape investing.com
     investing_filter = []
@@ -163,8 +164,6 @@ if __name__ == "__main__":
 
     symbol_list = df_stock.loc[investing_filter].index.tolist()
     try:
-        from_date = start.strftime('%d/%m/%Y')
-        to_date = end.strftime('%d/%m/%Y')
         if len(symbol_list) > 0:
             for symbol in tqdm(pool.imap_unordered(partial(scrape_investing, df_stock, from_date, to_date), symbol_list), total=len(symbol_list)):
                 pass
@@ -216,4 +215,5 @@ if __name__ == "__main__":
                 pass
             print('\nFinished scrape jitta.com')
 
+    pool.close()
     print('\nFinished')
